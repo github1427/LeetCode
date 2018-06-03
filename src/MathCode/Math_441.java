e187512d59dd1d42d2d5de56fa0971413774956d
damon <118112592@qq.com>
2018-06-03 19:30:16 +0800

    Math_441 AC

---
+++ src/MathCode/Math_441.java
@@ -0,0 +1,48 @@
+package MathCode;
+
+/**
+ * 排列硬币
+ * 你总共有 n 枚硬币，你需要将它们摆成一个阶梯形状，第 k 行就必须正好有 k 枚硬币。
+ * <p>
+ * 给定一个数字 n，找出可形成完整阶梯行的总行数。
+ * <p>
+ * n 是一个非负整数，并且在32位有符号整型的范围内。
+ * <p>
+ * 示例 1:
+ * <p>
+ * n = 5
+ * <p>
+ * 硬币可排列成以下几行:
+ * ¤
+ * ¤ ¤
+ * ¤ ¤
+ * <p>
+ * 因为第三行不完整，所以返回2.
+ * 示例 2:
+ * <p>
+ * n = 8
+ * <p>
+ * 硬币可排列成以下几行:
+ * ¤
+ * ¤ ¤
+ * ¤ ¤ ¤
+ * ¤ ¤
+ * <p>
+ * 因为第四行不完整，所以返回3.
+ */
+public class Math_441 {
+    public static void main(String[] args) {
+        System.out.println(arrangeCoins(8));
+    }
+
+    public static int arrangeCoins(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n - i >= 0) {
+                count++;
+                n = n - i;
+            }
+        }
+        return count;
+    }
+}
